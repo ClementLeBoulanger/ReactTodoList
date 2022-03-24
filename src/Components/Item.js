e71101e6db0ff7cadede4971dc14ee2c71a54bb1
Clement Le Boulanger <hello@clement-leboulanger.com>
2022-03-24 19:31:33 +0100

    Add Form.js, Item.js and Bootstrap

---
+++ src/Components/Item.js
@@ -0,0 +1,10 @@
+
+export default function Item() {
+  return (
+    <li className="d-flex justify-content-between align-items-center p-2 mb-3">
+      <div className="p-3">Text</div>
+      <button className="btn btn-danger p-2 h-50">Supprimer</button>
+
+    </li>
+  )
+}
